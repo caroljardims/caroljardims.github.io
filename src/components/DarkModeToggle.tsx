5c8b19b49a8b4ada86150d3137c4cba83e61353d
Carol <caroljardims@gmail.com>
2026-04-10 19:21:33 -0300

    improve: add colors to dark mode toggle icons

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -32,16 +32,12 @@ export const DarkModeToggle: React.FC = () => {
       aria-label="Toggle dark mode"
     >
       {isDark ? (
-        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+        <svg className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20">
           <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
         </svg>
       ) : (
-        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-          <path
-            fillRule="evenodd"
-            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm0 12a5 5 0 110-10 5 5 0 010 10zm0-2a3 3 0 100-6 3 3 0 000 6zm0-11a1 1 0 00-1-1H5a1 1 0 00-1 1v1a1 1 0 102 0V3h4a1 1 0 001-1zm12 2a1 1 0 01-1 1h-1a1 1 0 110-2h1a1 1 0 011 1zm-1 9a1 1 0 00-1-1h-1a1 1 0 110-2h1a1 1 0 001 1z"
-            clipRule="evenodd"
-          />
+        <svg className="w-5 h-5 text-amber-500" fill="currentColor" viewBox="0 0 20 20">
+          <path fillRule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm0 12a5 5 0 110-10 5 5 0 010 10zm0-2a3 3 0 100-6 3 3 0 000 6zm0-11a1 1 0 00-1-1H5a1 1 0 00-1 1v1a1 1 0 102 0V3h4a1 1 0 001-1zm12 2a1 1 0 01-1 1h-1a1 1 0 110-2h1a1 1 0 011 1zm-1 9a1 1 0 00-1-1h-1a1 1 0 110-2h1a1 1 0 001 1z" clipRule="evenodd" />
         </svg>
       )}
     </button>
